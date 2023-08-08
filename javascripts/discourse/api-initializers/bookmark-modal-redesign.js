367e1d6d96ce2dfc8943bd15dd11c051a340da6a
Martin Brennan <martin@discourse.org>
2023-08-08 13:04:30 +1000

    DEV: Initial commit

---
+++ javascripts/discourse/api-initializers/bookmark-modal-redesign.js
@@ -0,0 +1,72 @@
+import { apiInitializer } from "discourse/lib/api";
+import {
+  CLOSE_INITIATED_BY_BUTTON,
+  CLOSE_INITIATED_BY_ESC,
+} from "discourse/components/d-modal";
+import Bookmark from "discourse/models/bookmark";
+import { BookmarkFormData } from "discourse/lib/bookmark";
+import BookmarkRedesignModal from "../components/modal/bookmark-redesign";
+
+export default apiInitializer("0.11.1", (api) => {
+  const modalService = api.container.lookup("service:modal");
+
+  api.addPostMenuButton("coffee", () => {
+    return {
+      action: "openRedesignedBookmarkModal",
+      icon: "adjust",
+      className: "open-redesigned-bookmark-modal",
+      title: "post.bookmark",
+      position: "last",
+    };
+  });
+
+  api.attachWidgetAction("post", "openRedesignedBookmarkModal", function () {
+    const post = this.model;
+    const bookmarkForPost = post.topic.bookmarks.find(
+      (bookmark) =>
+        bookmark.bookmarkable_id === post.id &&
+        bookmark.bookmarkable_type === "Post"
+    );
+    _modifyPostBookmark(
+      modalService,
+      bookmarkForPost || Bookmark.createFor(this.currentUser, "Post", post.id),
+      post
+    );
+  });
+});
+
+function _modifyPostBookmark(modalService, bookmark, post) {
+  modalService
+    .show(BookmarkRedesignModal, {
+      model: {
+        bookmark: new BookmarkFormData(bookmark),
+        context: post,
+        afterSave: (savedData) => {
+          this._syncBookmarks(savedData);
+          this.model.set("bookmarking", false);
+          post.createBookmark(savedData);
+          this.model.afterPostBookmarked(post, savedData);
+          return [post.id];
+        },
+        afterDelete: (topicBookmarked, bookmarkId) => {
+          this.model.removeBookmark(bookmarkId);
+          post.deleteBookmark(topicBookmarked);
+        },
+      },
+    })
+    .then((closeData) => {
+      if (!closeData) {
+        return;
+      }
+
+      if (
+        closeData.closeWithoutSaving ||
+        closeData.initiatedBy === CLOSE_INITIATED_BY_ESC ||
+        closeData.initiatedBy === CLOSE_INITIATED_BY_BUTTON
+      ) {
+        post.appEvents.trigger("post-stream:refresh", {
+          id: bookmark.bookmarkable_id,
+        });
+      }
+    });
+}
---
+++ javascripts/discourse/components/modal/bookmark-redesign.js
@@ -0,0 +1,153 @@
+import Component from "@glimmer/component";
+import { AUTO_DELETE_PREFERENCES } from "discourse/models/bookmark";
+import { parseCustomDatetime } from "discourse/lib/time-utils";
+import discourseLater from "discourse-common/lib/later";
+import { extractError } from "discourse/lib/ajax-error";
+import { sanitize } from "discourse/lib/text";
+import { ajax } from "discourse/lib/ajax";
+import { CLOSE_INITIATED_BY_CLICK_OUTSIDE } from "discourse/components/d-modal";
+import { action } from "@ember/object";
+import I18n from "I18n";
+import { and, notEmpty } from "@ember/object/computed";
+import { inject as service } from "@ember/service";
+import { tracked } from "@glimmer/tracking";
+
+export default class BookmarkRedesignModal extends Component {
+  @service dialog;
+  @service currentUser;
+  @service site;
+
+  @tracked postDetectedLocalDate = null;
+  @tracked postDetectedLocalTime = null;
+  @tracked postDetectedLocalTimezone = null;
+  @tracked prefilledDatetime = null;
+  @tracked flash = null;
+  @tracked userTimezone = this.currentUser.user_option.timezone;
+  @tracked showOptions = this.args.model.bookmark.id ? true : false;
+
+  @notEmpty("userTimezone") userHasTimezoneSet;
+
+  @notEmpty("bookmark.id") editingExistingBookmark;
+
+  @and("bookmark.id", "bookmark.reminderAt") existingBookmarkHasReminder;
+
+  @tracked _closeWithoutSaving = false;
+  @tracked _savingBookmarkManually = false;
+  @tracked _saving = false;
+  @tracked _deleting = false;
+
+  get bookmark() {
+    return this.args.model.bookmark;
+  }
+
+  get modalTitle() {
+    return I18n.t(this.bookmark.id ? "bookmarks.edit" : "bookmarks.create");
+  }
+
+  get bookmarkAfterNotificationModes() {
+    return Object.keys(AUTO_DELETE_PREFERENCES).map((key) => {
+      return {
+        value: AUTO_DELETE_PREFERENCES[key],
+        name: I18n.t(`bookmarks.auto_delete_preference.${key.toLowerCase()}`),
+      };
+    });
+  }
+
+  @action
+  didInsert() {
+    discourseLater(() => {
+      if (this.site.isMobileDevice) {
+        document.getElementById("bookmark-name").blur();
+      }
+    });
+
+    if (!this.args.model.bookmark.id) {
+      document.getElementById("tap_tile_none").classList.add("active");
+    }
+
+    this.#initializeExistingBookmarkData();
+  }
+
+  @action
+  closingModal(closeModalArgs) {
+    // If the user clicks outside the modal we save automatically for them,
+    // as long as they are not already saving manually or deleting the bookmark.
+    if (
+      closeModalArgs.initiatedBy === CLOSE_INITIATED_BY_CLICK_OUTSIDE &&
+      !this._closeWithoutSaving &&
+      !this._savingBookmarkManually
+    ) {
+      this.#saveBookmark()
+        .catch((e) => this.#handleSaveError(e))
+        .then(() => {
+          this.args.closeModal(closeModalArgs);
+        });
+    } else {
+      this.args.closeModal(closeModalArgs);
+    }
+  }
+
+  @action
+  closeWithoutSavingBookmark() {
+    this._closeWithoutSaving = true;
+    this.args.closeModal({ closeWithoutSaving: this._closeWithoutSaving });
+  }
+
+  @action
+  saveAndClose() {
+    this.flash = null;
+    if (this._saving || this._deleting) {
+      return;
+    }
+
+    this._saving = true;
+    this._savingBookmarkManually = true;
+    return this.#saveBookmark()
+      .then(() => this.args.closeModal())
+      .catch((error) => this.#handleSaveError(error))
+      .finally(() => {
+        this._saving = false;
+      });
+  }
+
+  #saveBookmark() {
+    if (this.editingExistingBookmark) {
+      return ajax(`/bookmarks/${this.bookmark.id}`, {
+        type: "PUT",
+        data: this.bookmark.saveData,
+      }).then(() => {
+        this.args.model.afterSave?.(this.bookmark.saveData);
+      });
+    } else {
+      return ajax("/bookmarks", {
+        type: "POST",
+        data: this.bookmark.saveData,
+      }).then((response) => {
+        this.bookmark.id = response.id;
+        this.args.model.afterSave?.(this.bookmark.saveData);
+      });
+    }
+  }
+
+  #handleSaveError(error) {
+    this._savingBookmarkManually = false;
+    if (typeof error === "string") {
+      this.flash = sanitize(error);
+    } else {
+      this.flash = sanitize(extractError(error));
+    }
+  }
+
+  #initializeExistingBookmarkData() {
+    if (!this.existingBookmarkHasReminder || !this.editingExistingBookmark) {
+      return;
+    }
+
+    this.prefilledDatetime = this.bookmark.reminderAt;
+    this.bookmark.selectedDatetime = parseCustomDatetime(
+      this.prefilledDatetime,
+      null,
+      this.userTimezone
+    );
+  }
+}
